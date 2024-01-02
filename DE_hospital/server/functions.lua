002f6fb21f41ee79f63cc964f9af447864bb5578
DimeloEli <108193709+DimeloEli@users.noreply.github.com>
2024-01-02 14:57:58 -0500

    Add files via upload

--- DE_hospital/server/functions.lua
+++ DE_hospital/server/functions.lua
@@ -2,8 +2,9 @@ Notify = function(source, type, text)
     if Config.Notify == 'dlrms' then
         TriggerClientEvent('dlrms_notify', source, type, text)
     elseif Config.Notify == 'mythic' then
-        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = type, text = message })
+        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = type, text = text })
     elseif Config.Notify == 'other' then
         -- notify stuff here
+        TriggerClientEvent('ox_lib:notify', source, {type = type, description = text})
     end
 end
